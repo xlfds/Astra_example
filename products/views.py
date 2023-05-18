d5881fc8ed37151b005238a2a98f8de01dda778f
Ing. Petr Pecha <nejlepsitextovyeditorjevim@gmail.com>
2023-05-18 11:04:44 +0200

    products: download and unzip export file

--- products/views.py
+++ products/views.py
@@ -1,13 +1,29 @@
+import requests
+import io
+
+from zipfile import ZipFile
+
 from django.shortcuts import render
 
 def home(request):
     print ("home is home")
     return render(request, "products/home.html")
 
+def download_export(url="https://www.retailys.cz/wp-content/uploads/astra_export_xml.zip"):
+    try:
+        file = requests.get(url, stream=True)
+        zip_file = ZipFile(io.BytesIO(file.content), 'r')
+        xml_name = zip_file.namelist()[0]
+        raw_xml = zip_file.read(xml_name)
+    except IndexError:
+        return None
+
+    return raw_xml
+
 def task1(request):
     context = dict()
     context['task'] = "Task3"
-    context['total_sum'] = 20
+    context['total_sum'] = download_export()
     return render(request, "products/home.html", context)
 
 def task2(request):
